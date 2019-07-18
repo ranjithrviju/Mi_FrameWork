713da162b0e60f8b5da3cbd0a46d65a49369ac77
ranjith.r <ranjith.r@SLKGROUPGB-L107.slk-itesblr.com>
2019-07-18 15:32:49 +0530

    Selenium FrameWork

--- src/generic/CommonClass.java
+++ src/generic/CommonClass.java
@@ -1,10 +1,8 @@
 package generic;
 import java.io.FileInputStream;
-import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -13,16 +11,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-import utilities.ExcelUtility;
 import utilities.TestUtils;
 
 public class CommonClass implements IConstants {
