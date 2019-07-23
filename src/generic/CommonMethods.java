5b0e9bccd7182f5e091cf7487e780db8e37bd34a
ranjith.r <ranjith.r@SLKGROUPGB-L107.slk-itesblr.com>
2019-07-23 17:48:32 +0530

    Selenium FrameWork

--- src/generic/CommonMethods.java
+++ src/generic/CommonMethods.java
@@ -17,9 +17,9 @@ public class CommonMethods extends CommonClass{
 	private static Logger log=Logger.getLogger("Common Methods");
 	private  WebElement ele;
 	private JavascriptExecutor js=(JavascriptExecutor) driver;
-	
+
 	//-----------------------------------------------------------------------------------------CLICK()-----------------------------------------------------------------------------------------------------------------//
-	
+
 	public  void clickEle(String locator) {
 		try {
 			ele=getElement(locator);
@@ -31,9 +31,9 @@ public class CommonMethods extends CommonClass{
 			e.printStackTrace();
 		}
 	}
-	
+
 	//---------------------------------------------------------------------------------SENDKEYS()-----------------------------------------------------------------------------------------------------------------//
-	
+
 	public void enterText(String locator, String data) {
 		try {
 			ele=getElement(locator);
@@ -45,23 +45,30 @@ public class CommonMethods extends CommonClass{
 			e.printStackTrace();
 		}
 	}
-	
+
 	//---------------------------------------------------------------------------------MOVE TO ELEMENT()-----------------------------------------------------------------------------------------------------------------//
-	
-	public void mouseHover(WebDriver driver, String locator) {
+
+	public void actionsClass(WebDriver driver, String locator,String action) {
 		try {
 			Actions act=new Actions(driver);
-			act.moveToElement(getElement(locator)).build().perform();
-			log.info("Mouse Hovering to "+locator);
-			test.log(Status.INFO, "Mouse Hover on "+locator);
+			if(action=="moveToElement") {
+				act.moveToElement(getElement(locator)).build().perform();
+				log.info("Mouse Hovering to "+locator);
+				test.log(Status.INFO, "Mouse Hover on "+locator);
+			}
+			if(action=="click") {
+				act.moveToElement(getElement(locator)).click().build().perform();
+				log.info("Mouse Hovering to "+locator);
+				test.log(Status.INFO, "Mouse Hover on "+locator);
+			}
 		} catch (Exception e) {
 			log.info("Cannot Mouse Hover to +"+locator+" :    "+e.getMessage());
 			e.printStackTrace();
 		}
 	}
-	
+
 	//---------------------------------------------------------------------------------DATE SELECTER-----------------------------------------------------------------------------------------------------------------//
-	
+
 	public String datePicker(int days) {
 		String datePick=null;
 		LocalDate now = LocalDate.now();
@@ -70,52 +77,52 @@ public class CommonMethods extends CommonClass{
 		log.info("Date to be selected is : "+datePick);
 		return datePick ;
 	}
-	
+
 	//---------------------------------------------------------------------------------SELECT CLASS-----------------------------------------------------------------------------------------------------------------//
-	
+
 	public Select dropDown(String locator) {
 		Select s = new Select(getElement(locator));
 		log.info("Created object of drop down "+locator);
 		return s;
 	}
-	
+
 	//---------------------------------------------------------------------------------GET FIRST SELECTED OPTION-----------------------------------------------------------------------------------------------------------------//
-	
+
 	public String getFirstSelectedItem(String locator) {
 		String option=null;
 		Select s = new Select(getElement(locator));
 		option = s.getFirstSelectedOption().getText();
 		return option;
 	}
-	
+
 	//---------------------------------------------------------------------------------JAVASCRIPT CLICK-----------------------------------------------------------------------------------------------------------------//
-	
+
 	public void javaScriptClick(String locator) {
 		js.executeScript("arguments[0].click()", getElement(locator));
 		log.info("Clicking on "+locator);
 		test.log(Status.INFO, "Click on "+locator);
 	}
-	
+
 	//---------------------------------------------------------------------------------JAVASCRIPT ENTER TEXT-----------------------------------------------------------------------------------------------------------------//
-	
+
 	public void javaScriptEnterText(String locator,String data) {
 		js.executeScript("arguments[0].value='"+data+"'", getElement(locator));
 		log.info("Entered "+data+" in "+locator);
 		test.log(Status.INFO, "Enter "+data+" in "+locator);
 	}
-	
+
 	//---------------------------------------------------------------------------------GET LIST-----------------------------------------------------------------------------------------------------------------//
-		
+
 	public  List<String> getList(List<WebElement> eleList ) {
-			List<String> list=new ArrayList<>();
-			for (WebElement ele : eleList) {
-				list.add(ele.getText());
-			}
-			return list;
+		List<String> list=new ArrayList<>();
+		for (WebElement ele : eleList) {
+			list.add(ele.getText());
 		}
-	
+		return list;
+	}
+
 	//---------------------------------------------------------------------------------ACCEPT ALERT-----------------------------------------------------------------------------------------------------------------//
-	
+
 	public Alert acceptAlert(WebDriver driver) {
 		return driver.switchTo().alert();
 	}
